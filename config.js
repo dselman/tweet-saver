4c113bd9b9fe066559476ff9086e56addcef55d0
Dan Selman <danscode@selman.org>
2020-04-04 13:57:59 +0100

    (debug) ssl
    Signed-off-by: Dan Selman <danscode@selman.org>

--- config.js
+++ config.js
@@ -3,10 +3,11 @@ require('dotenv').config()
 const { Pool } = require('pg')
 const isProduction = process.env.NODE_ENV === 'production'
 
-const connectionString = process.env.DATABASE_URL + '&ssl=true';
+const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`
 
 const pool = new Pool({
-  connectionString: connectionString
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
 })
 
 module.exports = { pool }

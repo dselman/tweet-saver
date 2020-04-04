32bf619842dc942a18499235a4406bf7e383ab30
Dan Selman <danscode@selman.org>
2020-04-04 13:38:09 +0100

    (wip)
    Signed-off-by: Dan Selman <danscode@selman.org>

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const { Pool } = require('pg')
 const isProduction = process.env.NODE_ENV === 'production'
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL + '&ssl=true';
 
 const pool = new Pool({
   connectionString: connectionString,

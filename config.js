d996edbb6e897d0ccc33988100a0b6f903d6fa18
Dan Selman <danscode@selman.org>
2020-04-04 14:04:55 +0100

    (debug) ssl
    Signed-off-by: Dan Selman <danscode@selman.org>

--- config.js
+++ config.js
@@ -5,9 +5,12 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}?sslmode=require`;
 
-const pool = new Pool({
+const config = {
   connectionString: isProduction ? `${process.env.DATABASE_URL}?sslmode=require` : connectionString,
   ssl: isProduction,
-})
+}
+
+console.log(JSON.stringify(config, null,2));
+const pool = new Pool(config)
 
 module.exports = { pool }

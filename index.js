1625c4300e1ce46a6d891af9b78d22468c5ac355
Dan Selman <danscode@selman.org>
2020-04-06 13:44:58 +0100

    (feat) add exclusion words
    Signed-off-by: Dan Selman <danscode@selman.org>

--- index.js
+++ index.js
@@ -39,6 +39,8 @@ if(process.env.DELETE_ALL) {
       });
 }
 
+const excludes = process.env.EXCLUDE ? process.env.EXCLUDE.toUpperCase().split(',') : [];
+
 console.log(`Tracking tweets: ${process.env.TRACK}`);
 const stream = client.stream('statuses/filter', {track: process.env.TRACK});
 stream.on('data', function(event) {
@@ -49,29 +51,40 @@ stream.on('data', function(event) {
       // console.log(JSON.stringify(event, null, 2));
       // console.log('***\n\n\n\n');
       const tweetText = event.extended_tweet ? event.extended_tweet.full_text : event.text;
-      const tweet = {
-        date: event.created_at,
-        extraText: tweetText,
-        summary: `@${event.user.screen_name}`,
-        meta: `Followers: ${event.user.followers_count}` + (event.user.location ? ` from ${event.user.location}` : '')
-      };
-      events.unshift(tweet);
-      events.length = Math.min(100, events.length);
-      broadcast(tweet);
-
-      if(process.env.LOGDB) {
-          pool.query('INSERT INTO tweets (created_at, id_str, screen_name, user_location, user_followers_count, tweet_text, truncated) VALUES ($1, $2, $3, $4, $5, $6, $7)', 
-          [event.created_at, event.id_str, event.user.screen_name, event.user.location, event.user.followers_count, tweetText, event.truncated], error => {
-          if (error) throw error;
-        });
+      const tweetTextUpperCase = tweetText.toUpperCase();
+      for( let n=0; n < excludes.length; n++) {
+        if(tweetTextUpperCase.includes(excludes[n])) {
+          // console.log(`Excluded ${tweetText}`);
+          log = false;
+          break;
+        }
       }
 
-      if(process.env.LOG_FILE) {
-        fs.appendFile('tweets.csv', `${event.created_at},${event.id_str},${event.user.screen_name},${event.user.location},${event.user.followers_count},${tweetText},${event.truncated}\n`, function (err) {
-          if (err) throw err;
-        });
+      if(log) {
+        const tweet = {
+          date: event.created_at,
+          extraText: tweetText,
+          summary: `@${event.user.screen_name}`,
+          meta: `Followers: ${event.user.followers_count}` + (event.user.location ? ` from ${event.user.location}` : '')
+        };
+        events.unshift(tweet);
+        events.length = Math.min(100, events.length);
+        broadcast(tweet);
+  
+        if(process.env.LOGDB) {
+            pool.query('INSERT INTO tweets (created_at, id_str, screen_name, user_location, user_followers_count, tweet_text, truncated) VALUES ($1, $2, $3, $4, $5, $6, $7)', 
+            [event.created_at, event.id_str, event.user.screen_name, event.user.location, event.user.followers_count, tweetText, event.truncated], error => {
+            if (error) throw error;
+          });
+        }
+  
+        if(process.env.LOG_FILE) {
+          fs.appendFile('tweets.csv', `${event.created_at},${event.id_str},${event.user.screen_name},${event.user.location},${event.user.followers_count},${tweetText},${event.truncated}\n`, function (err) {
+            if (err) throw err;
+          });
+        }
+        process.stdout.write('.');  
       }
-      process.stdout.write('.');
     }
   }
 });
